82d8b99a2a2e942758a8b6ac1eb56889c9d20b86
byebyebi1 <karpelevi4@gmail.com>
2020-10-18 17:18:01 +0300

    добавление русского языка и огранчений на функции

--- Homework2/Homework2.cpp
+++ Homework2/Homework2.cpp
@@ -3,20 +3,23 @@
 using namespace std;
 
 int main() {
+
+	setlocale(LC_ALL, "Russian");
+
 	// Задача «Конус». Вычислить объем и полную поверхность усеченного конуса.
 	double h, r1, r2, S, l, V;
 	double pi = 2 * asin(1.0);
-	cout << "Enter height: ";
+	cout << "Введите высоту: ";
 	cin >> h;
-	cout << "Enter first radius: ";
+	cout << "Введите первый радиус: ";
 	cin >> r1;
-	cout << "Enter second radius: ";
+	cout << "Ввeдите второй радиус: ";
 	cin >> r2;
-	l = sqrt(h * h + (r1 - r2) * (r1));
+	l = sqrt(h * h + (r1 - r2) * (r1 - r2));
 	V = pi * h * (r1 * r1 + r1 * r2 + r2 * r2) / 3;
 	S = pi * (r1 * r1 + l * (r1 + r2) + r2 * r2);
-	cout << "Volume = " << V;
-	cout << "\nSurface = " << S << endl << endl;
+	cout << "Объём = " << V;
+	cout << "\nПолная поверхность = " << S << endl << endl;
 
 	// Задача «Разветвление». Для произвольных x и a вычислить.
 	double x, a, w;
@@ -25,13 +28,19 @@ int main() {
 	cout << "a: ";
 	cin >> a;
 	x = abs(x);
-	if (x < 1) {
-		w = a * log(x);
+	if ((!x)||(x * x > a)) {
+		cout << "Невозможно вычислить.";
 	}
 	else {
-		w = sqrt(a - x * x);
+		if (x < 1) {
+			w = a * log(x);
+		}
+		else {
+			w = sqrt(a - x * x);
+		}
+		cout << "w = " << w;
 	}
-	cout << "w = " << w << endl << endl;
+	cout << endl << endl;
 
 	// Задача «Функция». Для произвольных x, y и b вычислить функцию.
 	double x1, y, b, z;
@@ -41,8 +50,14 @@ int main() {
 	cin >> y;
 	cout << "b: ";
 	cin >> b;
-	z = log(b - y) * sqrt(b - x);
-	cout << "z = " << z << endl << endl;
+	if ((b <= y) || (b < x)) {
+		cout << "Невозможно вычислить.";
+	}
+	else {
+		z = log(b - y) * sqrt(b - x);
+		cout << "z = " << z;
+	}
+	cout << endl << endl;
 
 	// Задача «Порядок». Распечатать 10 последовательных
 	// натуральных чисел в возрастающем порядке,
@@ -50,9 +65,14 @@ int main() {
 	unsigned int N;
 	cout << "Enter N: ";
 	cin >> N;
-	for (int i = 10; i--;) {
-		cout << N << " ";
-		N += 1;
+	if (N >= 4294967295) {
+		cout << "Введённое число слишком большое.";
+	}
+	else {
+		for (int i = 10; i--;) {
+			cout << N << " ";
+			N += 1;
+		}
 	}
 	cout << endl << endl;
 
@@ -60,8 +80,13 @@ int main() {
 	// при изменении x от - 4 до + 4 с шагом 0.5.
 	double f, k;
 	for (k = -4; k <= 4; k += 0.5) {
-		f = (k * k - 2 * k + 2) / (k - 1);
-		cout << "x: " << k << "\t\ty: " << f << endl;
+		if (k == 1) {
+			cout << "x: " << k << "\t\ty: нельзя делить на ноль" << endl;
+		}
+		else {
+			f = (k * k - 2 * k + 2) / (k - 1);
+			cout << "x: " << k << "\t\ty: " << f << endl;
+		}
 	}
 
 
